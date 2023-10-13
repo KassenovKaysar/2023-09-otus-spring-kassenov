ae30d96ebbd2919b2ea537e1fcf4ea9e15e906b1
KassenovKaysar <147826625+KassenovKaysar@users.noreply.github.com>
2023-10-13 18:36:24 +0600

    homework 01 added

---
+++ hw01/readme.txt
@@ -0,0 +1,2 @@
+Otus spring framework HW01
+Group 2023-09
---
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,41 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        try {
+            final String testFileName = fileNameProvider.getTestFileName();
+            InputStream in = getClass().getResourceAsStream(testFileName);
+            List<QuestionDto> questions = new CsvToBeanBuilder(new InputStreamReader(in))
+                    .withType(QuestionDto.class)
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .build()
+                    .parse();
+            return questions.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (RuntimeException e) {
+            throw new QuestionReadException("questions couldn't be read", e);
+        }
+        // Использовать CsvToBean
+        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
+        // Использовать QuestionReadException
+
+
+    }
+}
